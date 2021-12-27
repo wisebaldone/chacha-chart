0c3544c5e5333a771a8dbfda7d292b11c1d58c65
Evan Hughes <ehugh1@gmail.com>
2021-12-28 01:51:29 +1000

    Adding a index to the fake api

--- src/gen.py
+++ src/gen.py
@@ -1,12 +1,28 @@
-import random
 import json
-import time
 import os
+import click
+import glob
+import datetime
 
 
 @click.command()
-def main():
-    print("I index json files into a single one.")
+@click.argument('path')
+def main(path: str):
+    print("I index json files.")
+    reports = glob.glob(f'{path}/*.json')
+
+    reports = [os.path.basename(report) for report in reports]
+    times = [ts.removesuffix('.json') for ts in reports]
+
+    dates = [datetime.datetime.strptime(ts, "%d-%m-%Y") for ts in times]
+    dates.sort()
+
+    recent = f'{dates[-1].strftime("%d-%m-%Y")}.json'
+    print(f'Most recent: {recent}')
+    result = {'recent': recent, 'reports': reports}
+
+    with open('reports/index.json', 'w') as file:
+        file.write(json.dumps(result, indent=4))
 
 
 if __name__ == '__main__':
