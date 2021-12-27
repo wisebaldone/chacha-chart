ff015d421eb755bdbdb167596781ff8fd04e2cf3
Evan Hughes <ehugh1@gmail.com>
2021-12-28 01:20:59 +1000

    Splitting workflows

---
+++ src/gen.py
@@ -0,0 +1,13 @@
+import random
+import json
+import time
+import os
+
+
+@click.command()
+def main():
+    print("I index json files into a single one.")
+
+
+if __name__ == '__main__':
+    main()
