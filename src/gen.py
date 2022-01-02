986e50d46789cf4431bf62f258deb405c58734e5
Evan Hughes <ehugh1@gmail.com>
2022-01-02 22:21:55 +1000

    quick fix

--- src/gen.py
+++ src/gen.py
@@ -14,16 +14,23 @@ def main(path: str):
     reports = [os.path.basename(report) for report in reports]
     times = [ts.removesuffix('.json') for ts in reports]
 
-    dates = [datetime.datetime.strptime(ts, "%d-%m-%Y") for ts in times]
+    dates = [datetime.datetime.strptime(ts, "%d-%m-%Y") for ts in times if is_date(ts)]
     dates.sort()
 
     recent = f'{dates[-1].strftime("%d-%m-%Y")}.json'
     print(f'Most recent: {recent}')
-    result = {'recent': recent, 'reports': reports}
+    result = {'recent': recent, 'reports': [f'{time}.json' for time in times if is_date(time)]}
 
     with open('reports/index.json', 'w') as file:
         file.write(json.dumps(result, indent=4))
 
+
+def is_date(input: str) -> bool:
+    try:
+        datetime.datetime.strptime(input, "%d-%m-%Y")
+    except:
+        return False
+    return True
 
 if __name__ == '__main__':
     main()
